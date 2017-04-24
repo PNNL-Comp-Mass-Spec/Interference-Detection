9d0a21d06848c8e728dcaf344f226f678de2de70
alchemistmatt <matthew.monroe@pnnl.gov>
2017-04-24 16:41:52 -0700

    Change from fields to properties

--- InterDetect/PrecursorInfo.cs
+++ InterDetect/PrecursorInfo.cs
@@ -5,36 +5,45 @@
     /// </summary>
     public class PrecursorInfo
     {
-        public double IsolationMass;
-        public int ChargeState;
-        public double IsolationWidth;
+        public double IsolationMass { get; private set; }
+
+        public int ChargeState { get; private set; }
+
+        public double IsolationWidth { get; private set; }
 
         /// <summary>
-        /// Interference score - larger is better, with a max of 1 and minimum of 0
+        /// Interference score: fraction of observed peaks that are from the precursor
+        /// Larger is better, with a max of 1 and minimum of 0
+        /// 1 means all peaks are from the precursor
         /// </summary>
-        public double Interference;
+        public double Interference { get; set; }
 
         /// <summary>
         /// The scan number of the product scan (NOT THE PRECURSOR SCAN NUMBER!)
         /// Used only for output
         /// </summary>
-        public int ScanNumber;
+        public int ScanNumber { get; set; }
 
         /// <summary>
         /// Populated by the algorithm
         /// </summary>
-        public double ActualMass;
+        public double ActualMass { get; set; }
 
-        public PrecursorInfo()
-        {
-        }
-
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public PrecursorInfo(double isolationMass, int chargeState, double isolationWidth)
         {
             IsolationMass = isolationMass;
             ChargeState = chargeState;
             IsolationWidth = isolationWidth;
         }
+
+        public void UpdateCharge(int newChargeState)
+        {
+            ChargeState = newChargeState;
+        }
+
         public override string ToString()
         {
             return string.Format("MS2 scan {0} @ {1:F2} m/z, charge {2}", ScanNumber, IsolationMass, ChargeState);
@@ -49,17 +58,25 @@
         /// <summary>
         /// Used to read the precursor scan data for the scan
         /// </summary>
-        public int PrecursorScanNumber;
+        public int PrecursorScanNumber { get; set; }
 
         /// <summary>
         /// Populated by the algorithm, and output to text
         /// </summary>
-        public double PrecursorIntensity;
+        public double PrecursorIntensity { get; set; }
 
         /// <summary>
         /// Read from raw file and output to text
         /// </summary>
-        public double IonCollectionTime;
+        public double IonCollectionTime { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public PrecursorIntense(double isolationMass, int chargeState, double isolationWidth)
+            : base(isolationMass, chargeState, isolationWidth)
+        {
+        }
 
         public override string ToString()
         {

70b5f242eca32a1f8727fa8a00901ec1060acf56
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-23 15:00:17 -0700

    Remove duplicate path from the distribution file

--- InterDetect/bin/Distribute_Files.bat
+++ InterDetect/bin/Distribute_Files.bat
@@ -1,12 +1,11 @@
 xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\Lib\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\bin\Console\Debug\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\bin\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDM_Plugin\AM_IDM_Plugin\bin\Debug\" /D /Y
 xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\MASICTest\bin\Debug\" /D /Y
-xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDM_Plugin\AM_IDM_Plugin\bin\Release\" /D /Y
 xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\Release\" /D /Y
+xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDM_Plugin\AM_IDM_Plugin\bin\Debug\" /D /Y
+xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDM_Plugin\AM_IDM_Plugin\bin\Release\" /D /Y
+xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\bin\" /D /Y
+xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\bin\Console\Debug\" /D /Y
+xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\Lib\" /D /Y
+xcopy Debug\InterDetect.dll "F:\Documents\Projects\DataMining\MASIC\MASICTest\bin\Debug\" /D /Y
 
 pause

2f59231704c447cacdd8a5c58c963e8e2996fb7f
alchemistmatt <matthew.monroe@pnnl.gov>
2020-11-20 16:58:01 -0800

    Suppress warnings

---
+++ InterDetect/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:InterDetect.IsosHandler.GetChargeState(System.Int32,System.Double,System.Int32@)~System.Boolean")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:InterDetect.InterferenceCalculator.ChargeStateGuesstimator(System.Double,System.Collections.Generic.List{InterDetect.Peak})~System.Int32")]
+[assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~M:InterDetect.InterferenceDetector.ParentInfoPass(System.Int32,System.Int32,System.String,System.String,System.Int32,System.Int32)~System.Collections.Generic.List{InterDetect.PrecursorIntense}")]

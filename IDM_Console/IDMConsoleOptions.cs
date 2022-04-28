b4141731973a6b6296c4e814623fe438520335e3
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-27 20:51:41 -0700

    Update Mage.dll and update program date

--- IDM_Console/IDMConsoleOptions.cs
+++ IDM_Console/IDMConsoleOptions.cs
@@ -9,7 +9,7 @@ namespace IDM_Console
     {
         // Ignore Spelling: Guesstimation, Tol
 
-        private const string PROGRAM_DATE = "April 22, 2022";
+        private const string PROGRAM_DATE = "April 27, 2022";
 
         /// <summary>
         /// Constructor
